8f27a65ae65f1fabc56388bfd0422011352e06a2
Yash Shah <mail@yashshah.com>
2015-12-03 18:56:47 -0600

    Move the elements from html to main.jsx

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -6,8 +6,12 @@ const Slider = require('material-ui/lib/slider');
 const Main = React.createClass({
   render() {
     return (
-      <div>
-        <Slider name="slider1" />
+      <div className="row">
+	      <div className="col s12 center white-text"><h1>Pay Only for what you use</h1></div>
+	      <div className="col s10" id="slider">
+	      	<Slider name="slider1" />
+	      </div>
+	      <div className="col s2 center white-text vertical-center"><h4 id="sliderValue">10M</h4></div>
       </div>
     );
   },
