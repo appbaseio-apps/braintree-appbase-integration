a3c96525625591e4927bc66193e4f188c9dc635f
Yash Shah <mail@yashshah.com>
2015-12-05 11:51:20 -0600

    Add numberFormater function

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -15,6 +15,15 @@ const Main = React.createClass({
       </div>
     );
   },
+	numberFormater: function(value){
+		if (value >= 1000000) {
+	       return (value / 1000000).toFixed(0) + 'M';
+	    }
+	    else if (value >= 1000) {
+	       return (value / 1000).toFixed(0) + 'K';
+	    }
+	    return value;
+	},
 });
 
 export default Main;
