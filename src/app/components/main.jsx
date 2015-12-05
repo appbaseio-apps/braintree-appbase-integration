73c2fea0bac72cfe54b1b32f19b4f12b2467e04d
Yash Shah <mail@yashshah.com>
2015-12-05 11:51:55 -0600

    Setup the slider

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -4,17 +4,14 @@ import React from 'react';
 const Slider = require('material-ui/lib/slider');
 
 const Main = React.createClass({
-  render() {
-    return (
-      <div className="row">
-	      <div className="col s12 center white-text"><h1>Pay Only for what you use</h1></div>
-	      <div className="col s10" id="slider">
-	      	<Slider name="slider1" />
-	      </div>
-	      <div className="col s2 center white-text vertical-center"><h4 id="sliderValue">10M</h4></div>
-      </div>
-    );
-  },
+
+	getInitialState: function() {
+		return {
+			sliderValue:'10k APIs',
+			plan: 'Free Plan',
+			sliderValueUnformated: 0 
+		};
+	},
 	numberFormater: function(value){
 		if (value >= 1000000) {
 	       return (value / 1000000).toFixed(0) + 'M';
@@ -24,6 +21,68 @@ const Main = React.createClass({
 	    }
 	    return value;
 	},
+	updatePricing: function(value){
+		let newPlan = ''
+		if (value >= 100000000) {
+	       newPlan = 'Enterprise Plan - $1999/month'
+	    }
+	    else if (value >= 50000000) {
+           newPlan = 'Premier Plan - $1299/month'
+        }
+    	else if (value >= 10000000) {
+           newPlan = 'Growth Plan - $799/month'
+        }
+        else if (value >= 1000000) {
+           newPlan = 'Startup Plan - $199/month'
+        }
+	    else if (value >= 100000) {
+           newPlan = 'Hacker Plan - $49/month'
+	    }
+	    else{
+	       newPlan = 'Free Plan'
+	    }
+	    this.setState({
+	    	plan:newPlan 
+	    });
+
+	},
+	updateSliderValue: function(event,value){
+		this.setState({
+			sliderValue: this.numberFormater(value*100000000)
+		});
+		this.setState({
+			sliderValueUnformated:value*100000000
+		});
+		this.updatePricing(value*100000000)
+	},
+	updateBraintreePlan: function(event){
+		let value = this.state.sliderValueUnformated
+		jQuery.ajax( {
+            url: 'https://scalr.api.appbase.io/braintree/test/1',
+            type: 'PUT',
+            data: '{ "count": '+value+'}',
+            beforeSend : function( xhr ) {
+                xhr.setRequestHeader( "Authorization", "Basic " + btoa("RweqvQFhv" + ":" + "9e57af31-90fd-4f09-b06d-8c3c2a53ca76"));
+            },
+            success: function( response ) {
+                console.log(response)
+            }
+		} );
+	},
+	render: function(){
+		return (
+			<div className="row">
+				<div className="col s12 center white-text"><h1>Select the number of API calls</h1></div>
+				<div className="col s10" id="slider">
+					<Slider name="slider1" step={0.00000001} defaultValue={0.0001} onChange={this.updateSliderValue} onDragStop={this.updateBraintreePlan}/>
+				</div>
+				<div className="col s2 center white-text vertical-center"><h4 id="sliderValue">{this.state.sliderValue}</h4></div>
+				<div className="col s12 center white-text"><h4>Your plan has been changed to</h4></div>
+				<div className="col s12 center white-text"><h2>{this.state.plan}</h2></div>
+			</div>
+			);
+	},
 });
 
 export default Main;
+
